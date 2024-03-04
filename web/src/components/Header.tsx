986defa6bc9379a16b1c19af2318b01615c08a3c
PiAreSquared <vishvib@gmail.com>
2024-03-04 21:51:20 +0000

    Added Highlights Page  - updated Header to reflect new name

--- web/src/components/Header.tsx
+++ web/src/components/Header.tsx
@@ -5,7 +5,7 @@ export default class Header extends React.Component {
         return (
             <header className="dark:bg-gray-800 py-4 rounded-lg shadow m-4">
                 <div className="container mx-auto">
-                    <h1 className="text-gray-400 dark:text-gray-300 text-3xl font-bold text-center">AI Sports Reels</h1>
+                    <h1 className="text-gray-400 dark:text-gray-300 text-3xl font-bold text-center">Clipper</h1>
                 </div>
             </header>
     );
